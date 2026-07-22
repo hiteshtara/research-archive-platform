82e31a279178ae9685f3d29e3662ffe45b453901
Hitesh Tara <hht179@gmail.com>
2026-07-21 21:18:49 -0400

    Expand Proposal Oracle metadata discovery

--- sql/extract/proposal/proposal_columns.sql
+++ sql/extract/proposal/proposal_columns.sql
@@ -3,11 +3,18 @@ SELECT
     column_id,
     column_name,
     data_type,
-    data_length
+    data_length,
+    data_precision,
+    data_scale,
+    nullable
 FROM all_tab_columns
 WHERE owner = 'KCOEUS'
   AND table_name IN (
+      'ACTIVITY_TYPE',
       'PROPOSAL',
-      'PROPOSAL_PERSONS'
+      'PROPOSAL_PERSONS',
+      'PROPOSAL_TYPE',
+      'SPONSOR',
+      'UNIT'
   )
 ORDER BY table_name, column_id;
